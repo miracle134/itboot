20d376c8391e91be8a5a9df7d4476f8c08938677
MiracleCat <miracle_1212@naver.com>
2023-05-24 11:56:57 +0900

    Initial commit

---
+++ src/main/java/com/mc/itboot/ItbootApplication.java
@@ -0,0 +1,13 @@
+package com.mc.itboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mc/itboot/ItbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.itboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
